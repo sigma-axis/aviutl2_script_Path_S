b6fd7e446f17d77fa1d9773cd885f5ee33481c64
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2025-11-24 08:26:04 +0900

    v1.10-for-beta21

--- Path_S.lua
+++ Path_S.lua
@@ -24,7 +24,7 @@ https://mit-license.org/
 ]]
 
 --
--- v1.10-rc1 (for beta21)
+-- v1.10 (for beta21)
 --
 
 local obj, math, tonumber, bit, ffi, buffer = obj, math, tonumber, bit, require("ffi"), require("string.buffer");
